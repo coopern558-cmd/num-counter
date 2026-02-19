2bdadbb2169d965c4caac59c9f039252b48a0bdd
coopern558-cmd <coopern558@gmail.com>
2026-02-19 12:31:32 +0000

    Add files via upload

---
+++ numcounter.py
@@ -0,0 +1,20 @@
+#imports
+import time
+
+#values
+value = 0
+max_num = 20
+speed_output = 0.5
+
+#def value + 1
+def numcounter():
+    global value
+    if value > max_num:
+        print ("value >",max_num)
+    else:
+        value = value + 1
+        time.sleep(speed_output)
+        print (value)
+        numcounter()
+
+numcounter()
